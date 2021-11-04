b1cdaafc92c17a48ed195998f456a69e96799d57
김준기 <gugi329@daum.net>
2021-11-04 23:26:12 +0900

    액티비티에서 ArrayList 생성 + RamenData들을 담아주는 작업  - 코드로 직접 타이핑 : 임시방편

--- app/src/main/java/com/neppplus/listviewpractice_20211103/MainActivity.kt
+++ app/src/main/java/com/neppplus/listviewpractice_20211103/MainActivity.kt
@@ -2,10 +2,24 @@ package com.neppplus.listviewpractice_20211103
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.listviewpractice_20211103.datas.RamenData
 
 class MainActivity : AppCompatActivity() {
+
+    val mRamenList = ArrayList<RamenData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRamenList.add( RamenData( "신라면", 1988, "농심" )  )
+        mRamenList.add( RamenData("진라면", 1998, "오뚜기") )
+        mRamenList.add( RamenData("왕뚜껑", 1996, "팔도") )
+        mRamenList.add( RamenData("스낵면", 1983, "오뚜기") )
+        mRamenList.add( RamenData("튀김우동", 1996, "농심") )
+        mRamenList.add( RamenData("참깨라면", 1993, "오뚜기") )
+        mRamenList.add( RamenData("팔도비빔면", 1977, "팔도") )
+        mRamenList.add( RamenData("진비빔면", 1992, "오뚜기") )
+
     }
 }
