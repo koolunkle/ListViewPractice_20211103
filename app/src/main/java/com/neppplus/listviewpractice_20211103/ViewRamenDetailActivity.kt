cd6e94a1dbbe870f49994811b75822e5c927a114
김준기 <gugi329@daum.net>
2021-11-04 23:47:00 +0900

    라면 상세보기 화면 추가

---
+++ app/src/main/java/com/neppplus/listviewpractice_20211103/ViewRamenDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.listviewpractice_20211103
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRamenDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_ramen_detail)
+    }
+}
