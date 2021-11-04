bf72801ded906133ec2e372bacb2336992cc852b
김준기 <gugi329@daum.net>
2021-11-04 23:28:15 +0900

    어댑터를 화면에서 사용할 준비 + 리스트뷰의 어댑터로, 우리가 만든 어댑터 연결

--- app/src/main/java/com/neppplus/listviewpractice_20211103/MainActivity.kt
+++ app/src/main/java/com/neppplus/listviewpractice_20211103/MainActivity.kt
@@ -2,11 +2,14 @@ package com.neppplus.listviewpractice_20211103
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.listviewpractice_20211103.adapters.RamenAdapter
 import com.neppplus.listviewpractice_20211103.datas.RamenData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mRamenList = ArrayList<RamenData>()
+    lateinit var mRamenAdapter : RamenAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,9 @@ class MainActivity : AppCompatActivity() {
         mRamenList.add( RamenData("참깨라면", 1993, "오뚜기") )
         mRamenList.add( RamenData("팔도비빔면", 1977, "팔도") )
         mRamenList.add( RamenData("진비빔면", 1992, "오뚜기") )
+
+        mRamenAdapter = RamenAdapter(this, R.layout.ramen_list_item, mRamenList)
+        ramenListView.adapter = mRamenAdapter
 
     }
 }
