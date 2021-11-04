8969d0c14267604b5624aa22d4c1d9cfd1cfa32d
김준기 <gugi329@daum.net>
2021-11-04 23:23:56 +0900

    convertView를 tempRow에 옮겨 담아서, 비어있는 상태라면 채워주기

--- app/src/main/java/com/neppplus/listviewpractice_20211103/adapters/RamenAdapter.kt
+++ app/src/main/java/com/neppplus/listviewpractice_20211103/adapters/RamenAdapter.kt
@@ -1,11 +1,27 @@
 package com.neppplus.listviewpractice_20211103.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.listviewpractice_20211103.R
 import com.neppplus.listviewpractice_20211103.datas.RamenData
 
 class RamenAdapter (
     val mContext: Context,
     val resId: Int,
     val mList: ArrayList<RamenData> ) : ArrayAdapter<RamenData>( mContext, resId, mList ) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.ramen_list_item, null)
+        }
+
+    }
+
 }
