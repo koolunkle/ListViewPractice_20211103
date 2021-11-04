4e2ea76bf84385f0cdc1dada66346fe1e8c2b383
김준기 <gugi329@daum.net>
2021-11-04 23:42:04 +0900

    리스트뷰의 이벤트 처리  - setOn"ItemLongClick"Listener  - 길게 눌린 경우 별도 처리 / true 리턴

--- app/src/main/java/com/neppplus/listviewpractice_20211103/MainActivity.kt
+++ app/src/main/java/com/neppplus/listviewpractice_20211103/MainActivity.kt
@@ -44,5 +44,17 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        ramenListView.setOnItemLongClickListener { adapterView, view, position, l ->
+
+            val longClickedRamen = mRamenList[position]
+
+            Toast.makeText(this, "${longClickedRamen.name} 길게 눌림", Toast.LENGTH_SHORT).show()
+
+
+//            Boolean (true / false)을 결과로 지정해줘야함.
+            return@setOnItemLongClickListener true
+        }
+
+
     }
 }
