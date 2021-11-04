645caf0615185f11fbcf1672ffe94cfb2d914158
김준기 <gugi329@daum.net>
2021-11-05 00:05:26 +0900

    라면 데이터에 기능을 추가  - getExpiry : 출시연도에 따른 유효기간 계산

--- app/src/main/java/com/neppplus/listviewpractice_20211103/datas/RamenData.kt
+++ app/src/main/java/com/neppplus/listviewpractice_20211103/datas/RamenData.kt
@@ -4,4 +4,16 @@ class RamenData (
     val name: String,
     val birthYear: Int,
     val address: String) {
+
+//    함수로 라면의 기능을 만들자.
+
+//    라면의 유효기간을 계산해서 알려주는 기능 - function
+
+    fun getExpiry(  ) : Int {
+
+        val Expiry = this.birthYear + 1
+
+        return Expiry
+    }
+
 }
